e21dd17fe29e84dacbade12e9b45014dfc7b6b8f
Stephen Traiforos <stephen@traiforos.com>
2025-09-03 16:47:11 -0400

    Remove unused resolver since we moved to deactive guards

--- projects/shared/src/lib/route/index.ts
+++ projects/shared/src/lib/route/index.ts
@@ -1,6 +1,5 @@
 export * from './step-route';
 export * from './resolver/logger-instrumentation-resolver';
-export * from './resolver/auth-resolver';
 export * from './guards/upload-valid-guard';
 export * from './guards/auth-valid-guard';
 export * from './guards/auth-deactivate-guard';
